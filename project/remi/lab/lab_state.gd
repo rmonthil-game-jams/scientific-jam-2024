0acbcb9aad7f039c66aa21e19a2da16ea9c5bd6c
Rémi Monthiller <monthir@git.inpt.fr>
2024-04-06 17:41:53 +0200

    removed debug

--- project/remi/lab/lab_state.gd
+++ project/remi/lab/lab_state.gd
@@ -2,8 +2,8 @@ extends Node
 
 var alexia_state: StringName = ""
 
-var ams_done: bool = true
+var ams_done: bool = false
 #var cuve_done: bool = false
-var microscope_done: bool = true
+var microscope_done: bool = false
 
 var remaining_ice_cores_in_the_fridge: int = 5
